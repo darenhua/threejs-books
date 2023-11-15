05c380471617b43f879a19317414d4967ef092c4
Daren Hua <59425042+darenhua@users.noreply.github.com>
2023-11-15 04:30:08 -0500

    analytics

--- src/index.jsx
+++ src/index.jsx
@@ -2,6 +2,13 @@ import React from "react";
 import { createRoot } from "react-dom/client";
 import App from "./App";
 import "./styles.css";
+import { Analytics } from "@vercel/analytics/react";
 
 const root = createRoot(document.getElementById("root"));
-root.render(<App />);
+const WithAnalytics = () => (
+  <>
+    <App />
+    <Analytics />
+  </>
+);
+root.render(<WithAnalytics />);
