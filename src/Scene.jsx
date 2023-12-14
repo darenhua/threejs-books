d47036de990e5efbd9a6e0045883a83f242e52f1
Daren Hua <59425042+darenhua@users.noreply.github.com>
2023-12-14 18:16:27 -0500

    finish data

--- src/Scene.jsx
+++ src/Scene.jsx
@@ -14,7 +14,7 @@ const Scene = () => {
     fov: 60,
   };
 
-  const { onClickAnywhere } = useContext(SceneContext);
+  const { onClickAnywhere, selected } = useContext(SceneContext);
 
   const text =
     "Language is important. Not all indigenous people identify with their traditions and values as a religion. For example, the Bribri people in Talamanca, Costa Rica generally prefer the terms “culture” and “tradition” over religion. Indigenous studies scholars like Philip P. Arnold use “Indigenous Values” rather than “Indigenous Religions” because values better represent the relationship that indigenous people have with their belief systems. \n One explanation that I found particularly interesting about the odds between indigeneity and religion is that the world religions like Christianity, Islam, Buddhism and more, tend to be centered around individual salvation. Meanwhile, indigenous life is centered around relationships/kinship. So, it is difficult to accept the theme that people opt into religions like Christianity in order to enter Heaven and save their own souls into a indigenous “religion”. \n Furthermore, the history between the world religons and indigenous people is bad. Missionaries attempt to erase “indigenous religions” and convert people. War and violence upon the Native Americans is justified by Christianity. Since indigenous people have had a bad experience with external organized religion, it makes sense that many do not agree with traditions being religion as well.";
@@ -25,7 +25,7 @@ const Scene = () => {
       <Center>
         <Bookshelf numBooks={data.length} data={data} />
       </Center>
-      <Modal data={data[2]} />
+      <Modal data={data[selected]} />
       <Environment preset='city' />
       <FX />
     </Canvas>
