bc15f15a100e7074a849595f6d95d3984c4b669f
Daren Hua <59425042+darenhua@users.noreply.github.com>
2023-12-14 14:41:08 -0500

    globals rafz fix

--- src/index.jsx
+++ src/index.jsx
@@ -1,6 +1,11 @@
 import React from "react";
 import { createRoot } from "react-dom/client";
 import { Analytics } from "@vercel/analytics/react";
+import { Globals } from "@react-spring/shared";
+
+Globals.assign({
+  frameLoop: "always",
+});
 
 import App from "./App";
 import "./styles.css";
